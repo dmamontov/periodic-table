ec3aa084a9657e08eace889e42093a9d0fa679e3
Dmitrii Mamontov <d@mamontov.tech>
2026-08-05 13:59:20 +0300

    Считать реальную высоту вьюпорта в JS вместо CSS vh/dvh для сайдбаров
    Три чисто CSS-варианта (100vh/100dvh, bottom:0, -webkit-fill-available)
    не давали стабильного результата в standalone-режиме на iOS - WKWebView
    там не всегда корректно считает vh/dvh для position:fixed. Вместо того
    чтобы гадать дальше, меряем реальную высоту через Visual Viewport API
    (window.visualViewport.height) в JS и публикуем как --real-vh, обновляя
    на resize/orientationchange. Панели используют height: var(--real-vh,
    100dvh) - CSS-юниты остаются фолбэком на случай если JS ещё не отработал.

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ import { installLocale } from './locales'
 import { installTheme } from './theme'
 import { initPwaUpdates } from './pwa'
 import { initPwaStandalone } from './utils/pwaStandalone'
+import { initRealViewportHeight } from './utils/viewportHeight'
 import { scheduleElementImagePreload } from './utils/elementImageCache'
 
 initPwaStandalone()
+initRealViewportHeight()
 initPwaUpdates()
 
 const app = createApp(App)
